cfdf6cb78b2d7ef75dfa23eac8b7ea6f276cf35f
HaonanWu <whn289467822@outlook.com>
2016-12-17 17:32:31 +0800

    net2 CNN

---
+++ Deep Learning/Lasagne/lasagne_mnist2.py
@@ -0,0 +1,119 @@
+# Using lasagne to fit mnist.
+# show how to use NeuralNet
+# HaonanWu, 12/17/16.
+
+import lasagne
+import theano
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
+import os
+from urllib import urlretrieve
+import gzip
+import pickle
+import numpy as np
+from matplotlib import pyplot
+
+DATA_URL = 'http://deeplearning.net/data/mnist/mnist.pkl.gz'
+DATA_FILENAME = './data/mnist.pkl.gz'
+
+def float32(k):
+    return np.cast['float32'](k)
+
+class AdjustVariable(object):
+    def __init__(self, name, start = 0.03, stop = 0.001):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.ls = None
+    def __call__(self, nn, train_history):
+        if self.ls is None:
+            self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
+        epoch = train_history[-1]['epoch']
+        new_value = float32(self.ls[epoch - 1])
+        getattr(nn, self.name).set_value(new_value)
+
+def pickle_load(f, encoding):
+    return pickle.load(f)
+
+def _load_data(url=DATA_URL, filename=DATA_FILENAME):
+    """Load data from `url` and store the result in `filename`."""
+    print 'filename for the minist datatset:',filename
+    if not os.path.exists(filename):
+        print("Downloading MNIST dataset")
+        urlretrieve(url, filename)
+
+    with gzip.open(filename, 'rb') as f:
+        return pickle_load(f, encoding='latin-1')
+
+
+def load():
+    """Get data with labels, split into training, validation and test set."""
+    data = _load_data()
+    X_train, y_train = data[0]
+    y_train = np.asarray(y_train,dtype='int32')
+    #X_valid, y_valid = data[1]
+    #X_test, y_test = data[2]
+    return X_train, y_train
+
+def load2d():
+    X, y = load()
+    X = X.reshape(-1, 1, 28, 28)
+    print 'size:',X.shape, y.shape, X.dtype, y.dtype
+    return X, y
+
+net2 = NeuralNet(
+    layers=[
+        ('input', layers.InputLayer),
+        ('conv1', layers.Conv2DLayer),
+        ('pool1', layers.MaxPool2DLayer),
+        ('conv2', layers.Conv2DLayer),
+        ('pool2', layers.MaxPool2DLayer),
+        ('conv3', layers.Conv2DLayer),
+        ('pool3', layers.MaxPool2DLayer),
+        ('hidden4', layers.DenseLayer),
+        ('hidden5', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+    input_shape=(None, 1, 28, 28),
+    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+    conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_pool_size=(2, 2),
+    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_pool_size=(2, 2),
+    hidden4_num_units=500, hidden5_num_units=500,
+    output_num_units=10, output_nonlinearity=lasagne.nonlinearities.softmax,
+
+	update=nesterov_momentum,
+    update_learning_rate=theano.shared(float32(0.03)),
+    update_momentum=theano.shared(float32(0.9)),
+
+    regression=False,
+    on_epoch_finished=[
+        AdjustVariable('update_learning_rate', start=0.03, stop=0.0001),
+        AdjustVariable('update_momentum', start=0.9, stop=0.999),
+        ],
+
+    regression=False,
+    max_epochs=10,
+    verbose=1,
+    )
+
+X, y = load2d()  # load 2-d data
+net2.fit(X, y)
+'''
+# Training for 1000 epochs will take a while.  We'll pickle the
+# trained model so that we can load it back later:
+import cPickle as pickle
+with open('net2.pickle', 'wb') as f:
+    pickle.dump(net2, f, -1)
+
+train_loss = np.array([i["train_loss"] for i in net2.train_history_])
+valid_loss = np.array([i["valid_loss"] for i in net2.train_history_])
+pyplot.plot(train_loss, linewidth=3, label="train")
+pyplot.plot(valid_loss, linewidth=3, label="valid")
+pyplot.grid()
+pyplot.legend()
+pyplot.xlabel("epoch")
+pyplot.ylabel("loss")
+pyplot.ylim(0, 1)
+pyplot.yscale("log")
+pyplot.show()
+'''
