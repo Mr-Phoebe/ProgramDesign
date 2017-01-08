2c7941e81bd1a4a323f69d8632c42fe1c533b05a
HaonanWu <whn289467822@outlook.com>
2017-01-08 19:22:44 +0800

    personal page

---
+++ Comprehensive Design and Experiment of Digital Media Content/PeronsalWeb/PeronsalWeb/urls.py
@@ -0,0 +1,28 @@
+"""PeronsalWeb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+import os
+from settings import STATICFILES_DIRS, BASE_DIR
+
+from personal import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^upload/$', views.upload),
+    url(r'^download/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(BASE_DIR, 'upload'), 'show_indexes': True}),
+]
---
+++ Comprehensive Design and Experiment of Digital Media Content/PeronsalWeb/personal/views.py
@@ -0,0 +1,46 @@
+#encoding: utf-8
+
+import os
+
+from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseRedirect
+
+from models import UploadFile
+# Create your views here.
+
+def index(request):
+    count = UploadFile.objects.count()
+    data = UploadFile.objects.all()
+    resp = []
+    for r in data:
+        res = {}
+        res['fileName'] = r.file_name
+        res['uploadTime'] = str(r.upload_time)
+        res['uploadPos'] = str(r.file_name)
+        resp.append(res)
+    return render(request, 'index.html', {'count': count, 'data': resp})
+
+def upload(request):
+    if request.method == 'POST':
+        try:
+            upload_file = request.FILES['file']
+            f = UploadFile()
+            filename = upload_file.name
+            f.file_name = filename
+            if not os.path.exists('upload/'):
+                os.mkdir('upload/')
+            print filename
+            filename = 'upload/' + filename
+            f.save()
+            with open(filename, 'wb+') as destination:
+                for r in upload_file.chunks():
+                    destination.write(r)
+                destination.close()
+            print "=======================success================="
+            return HttpResponseRedirect('/')
+        except Exception, ex:
+            print ex
+            return JsonResponse({'code': 0, 'reason': u'导入数据失败'})
+
+
+
