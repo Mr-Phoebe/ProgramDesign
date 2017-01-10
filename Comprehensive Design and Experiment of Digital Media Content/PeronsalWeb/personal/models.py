bf5b0bd158666df37d5e332f75a879facf94e2d2
HaonanWu <whn289467822@outlook.com>
2017-01-10 16:53:26 +0800

    add description of file

--- Comprehensive Design and Experiment of Digital Media Content/PeronsalWeb/personal/models.py
+++ Comprehensive Design and Experiment of Digital Media Content/PeronsalWeb/personal/models.py
@@ -5,13 +5,11 @@ from django import forms
 # Create your models here.
 
 class UploadFile(models.Model):
-
     file_name = models.CharField(max_length=32, null=True)
     upload_time = models.DateTimeField(auto_now=True)
 
 
 class UploadFileForm(forms.Form):
-
     title = forms.CharField(max_length=64)
     fileType = forms.CharField(max_length=32)
     theFile = forms.FileField()
