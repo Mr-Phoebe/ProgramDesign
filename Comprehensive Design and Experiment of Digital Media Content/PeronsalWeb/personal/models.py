17de346438e97f28adb1c8fcbc4bba1415fab54f
HaonanWu <whn289467822@outlook.com>
2017-01-10 18:22:53 +0800

    modify forms

--- Comprehensive Design and Experiment of Digital Media Content/PeronsalWeb/personal/models.py
+++ Comprehensive Design and Experiment of Digital Media Content/PeronsalWeb/personal/models.py
@@ -1,20 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django import forms
 # Create your models here.
 
 class UploadFile(models.Model):
     file_name = models.CharField(max_length=32, null=True)
     upload_time = models.DateTimeField(auto_now=True)
 
-
-
-class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=64)
-    fileType = forms.CharField(max_length=32)
-    theFile = forms.FileField()
-    description = forms.CharField(max_length=80)
 
 
 
