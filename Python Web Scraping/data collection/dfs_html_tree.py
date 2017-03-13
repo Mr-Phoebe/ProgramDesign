58eceab9dfabce448942567c3006503de0a09c82
HaonanWu <whn289467822@outlook.com>
2017-03-13 22:08:52 +0800

    Update dfs_html_tree.py

--- Python Web Scraping/data collection/dfs_html_tree.py
+++ Python Web Scraping/data collection/dfs_html_tree.py
@@ -6,7 +6,6 @@
 
 import urllib2
 import json
-import copy
 from bs4 import BeautifulSoup
 
 def get_bs4(url):
@@ -14,17 +13,6 @@ def get_bs4(url):
     headers = {'User-Agent':user_agent}
     request = urllib2.Request(url = url, headers = headers)
     response = urllib2.urlopen(request)
-    # print response.read()
-    # response = '''
-    #     <div class="article_title">
-    #     <span class="ico ico_type_Original"></span>
-    #     <h1>
-    #     <span class="link_title"><a href="/u013007900/article/details/52357067">
-    #             Leetcode 391. Perfect Rectangle            
-    #             </a></span>
-    #     </h1>
-    #     </div>
-    #'''
     return BeautifulSoup(response,"html.parser")
 
 def get_pos(now, contain, string):
@@ -40,33 +28,65 @@ def get_pos(now, contain, string):
             pass
         return 
 
-def get_class(now):
+def get_parent(now):
     for parent in now.parents: 
-        if parent is None: 
-            print parent
-        else: 
-            print parent.name
-            if parent.attrs.has_key('class') and parent.attrs['class'] != []:
-                print " ",parent.attrs['class']
+        if parent:
+            try:
+                if parent.attrs.has_key('class') and parent.attrs['class'] != []:
+                    return parent
+            except:
+                pass
+
+def print_tree(now):
+    try:
+        for child in now.children: 
+            print_tree(child)
+    except AttributeError as e:
+        try:
+            tmp = "" + now.string
+            tmp.strip()
+            f1 = open('test.txt','a+')
+            if tmp != '\n':
+                f1.write(tmp+"\n")
+            f1.close()
+        except:
+            pass
+        return 
+
+def get_all(now):
+    li = soup_packetpage.findAll(now.name, now.attrs)
+    for item in li:
+        try:
+            print_tree(item)
+        except:
+            pass
+
 
 
 if __name__ == '__main__':
+    import sys
+    reload(sys)             
+    sys.setdefaultencoding('utf-8')
     url = 'http://blog.csdn.net/u013007900?viewmode=contents'
-    string = u'Github计算contributions'
+    string = u'ACM'
     soup_packetpage = get_bs4(url)
     contain = []
     get_pos(soup_packetpage, contain, string)
     
+
     for pos in contain:
-        get_class(pos)
+        fa = get_parent(pos)
+        get_all(fa)
         print "********************"
 
-    li = soup_packetpage.findAll("span", class_=u'link_title')
-    for item in li:
-        try:
-            print item.string
-        except:
-            pass
+    # dic_test = {"class":["list_item", "list_view"]}
+
+    # li = soup_packetpage.findAll("div", dic_test)
+    # for item in li:
+    #     try:
+    #         print item.div.h1.span.string
+    #     except:
+    #         pass
 
 
 
